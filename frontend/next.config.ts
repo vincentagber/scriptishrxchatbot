51267ce8b42f1d37ae263b79b4f1f7531e774230
Vincent Agber <vincentagber74@gmail.com>
2025-12-16 14:18:24 +0100

    fix: enable trailing slash and relative image paths

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Cache Buster: 2025-12-13-CACHE-BUST-001
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
